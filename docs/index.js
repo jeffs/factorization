69d7496bbcf9081edfa064fe47d528c698faabe3
Jeff Schwab <jeff@unbuggy.com>
2023-02-22 07:15:04 -0500

    Export for GH Pages

--- docs/index.js
+++ docs/index.js
@@ -2,10 +2,17 @@ import FactorizationSection from "./components/FactorizationSection.js";
 import WasmFactorizationSection from "./components/WasmFactorizationSection.js";
 import { create } from "./elements.js";
 
+const description = `
+  This page shows two widgets that show the prime factors of given numbers.
+  Each widget delegates the computation to a separate web worker: One worker
+  does the computation in JavaScript, and the other in Rust. Aside from the
+  Rust worker, the page is pure, unbundled HTML/CSS/JS.`;
+
 document.body.append(
   create([
     "main",
-    ["h1", "Main"],
+    ["h1", "Factorization"],
+    ["p", description],
     FactorizationSection(),
     WasmFactorizationSection(),
   ])
